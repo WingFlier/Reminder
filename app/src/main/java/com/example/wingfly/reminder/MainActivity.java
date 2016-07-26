ab3f98fbcf0029fe3ae17520d60b0e855c6438fe
wingfly <tiko1581@gmail.com>
2016-07-27 02:30:15 +0400

    added item and header to navigationDrawer

--- app/src/main/java/com/example/wingfly/reminder/MainActivity.java
+++ app/src/main/java/com/example/wingfly/reminder/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wingfly.reminder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,14 +12,23 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initNavigationView();
+
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
     }
 
