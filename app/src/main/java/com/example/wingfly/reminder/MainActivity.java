9c1bb283d30cb1b3a4516217462734904519412b
wingfly <tiko1581@gmail.com>
2016-07-27 13:29:07 +0400

    adde tabs fragment and drawer layout click

--- app/src/main/java/com/example/wingfly/reminder/MainActivity.java
+++ app/src/main/java/com/example/wingfly/reminder/MainActivity.java
@@ -2,34 +2,76 @@ package com.example.wingfly.reminder;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.wingfly.reminder.adapter.TabsPagerFragmentAdapter;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int LAYOUT = R.layout.activity_main;
     private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         initNavigationView();
+        initTabs();
+
 
     }
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar,
+                R.string.view_navigation_open,
+                R.string.view_navigation_close);
+
+        //drawerLayout.setOnClickListener((View.OnClickListener) toggle);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()){
+                    case R.id.action_notification_item:
+                        showNotification();
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -52,5 +94,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void showNotification() {
+        viewPager.setCurrentItem(constants.TAB_TWO);
     }
 }
---
+++ app/src/main/java/com/example/wingfly/reminder/constants.java
@@ -0,0 +1,11 @@
+package com.example.wingfly.reminder;
+
+/**
+ * Created by wingfly on 7/27/16.
+ */
+public class constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
